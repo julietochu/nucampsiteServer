16d5bd831316ef2971628fe8e45f8e9fc1b088b8
juliet ochu <52784536+julietochu@users.noreply.github.com>
2022-05-26 17:29:24 -0600

    Passport.

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
 
 
 const mongoose = require('mongoose');
@@ -81,26 +84,7 @@ function auth(req, res, next) {
   }
 }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-    console.log(req.session);
-
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
-    }
-}
 function auth(req, res, next) {
   console.log(req.headers);
   const authHeader = req.headers.authorization;
@@ -123,6 +107,22 @@ function auth(req, res, next) {
       return next(err);
   }
 }
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');                    
+        err.status = 401;
+        return next(err);
+    } else {
+        return next();
+    }
+}
 
 app.use(auth);
 
@@ -133,6 +133,7 @@ app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+
 
 
 // catch 404 and forward to error handler
