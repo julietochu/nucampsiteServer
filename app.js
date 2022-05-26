a24cc84114d7618bf80edef13bd6301282ad2301
juliet ochu <52784536+julietochu@users.noreply.github.com>
2022-05-26 17:01:11 -0600

    Express Session Part 2.

--- app.js
+++ app.js
@@ -81,29 +81,48 @@ function auth(req, res, next) {
   }
 }
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// function auth(req, res, next) {
-//   console.log(req.headers);
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//       const err = new Error('You are not authenticated!');
-//       res.setHeader('WWW-Authenticate', 'Basic');
-//       err.status = 401;
-//       return next(err);
-//   }
+function auth(req, res, next) {
+    console.log(req.session);
 
-//   const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//   const user = auth[0];
-//   const pass = auth[1];
-//   if (user === 'admin' && pass === 'password') {
-//       return next(); // authorized
-//   } else {
-//       const err = new Error('You are not authenticated!');
-//       res.setHeader('WWW-Authenticate', 'Basic');      
-//       err.status = 401;
-//       return next(err);
-//   }
-// }
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
 
 app.use(auth);
 
